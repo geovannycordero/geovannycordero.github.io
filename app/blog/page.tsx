859b28deb7d1d78528f4462d0a9716fde7c053a3
Geovanny Cordero <gcordero@pernix-solutions.com>
2025-10-26 11:48:04 -0600

    Linter

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -161,7 +161,6 @@ export default async function BlogPage() {
     <div className='min-h-screen bg-background'>
       <EnhancedNavigation />
 
-
       <main className='pt-20'>
         <div className='container mx-auto px-4 sm:px-6 lg:px-8 py-12'>
           <div className='max-w-4xl mx-auto'>
